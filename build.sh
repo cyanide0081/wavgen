88609065e95c2031978689ee8129bdf880bda8d4
cyanide0081 <cyanide0081@gmail.com>
2023-12-23 20:22:57 -0300

    improve POSIX build script

--- build.sh
+++ build.sh
@@ -4,6 +4,11 @@
 gcc --version > /dev/null 2>&1 && CC="gcc"
 clang --version > /dev/null 2>&1 && CC="clang"
 
+if [ "$CC" = "" ]; then
+    printf "\033[1;41mERROR: no suitable compiler found (aborting)\033[0m\n"
+    exit 1
+fi
+
 DEFINES="-D_POSIX_C_SOURCE=200809L -D_DEFAULT_SOURCE"
 FLAGS="-std=c99 $DEFINES -Wall -Wextra -pedantic -lm"
 D_FLAGS="-g -ggdb"
