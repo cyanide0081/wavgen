9b01d6d1baa4115c928ecd7a64981f30b1653744
cyanide0081 <cyanide0081@gmail.com>
2023-12-23 23:41:02 -0300

    improve batch build script

--- build.cmd
+++ build.cmd
@@ -4,22 +4,27 @@
 where /Q gcc && (set "CC=gcc")
 where /Q clang && (set "CC=clang")
 
+if "%CC%" EQU "" (
+    echo [1;41mERROR: no suitable compiler found ^(aborting^)[0m
+    exit /b 1
+)
+
 set "FLAGS=-std=c99 -Wall -Wextra -pedantic -D_CRT_SECURE_NO_WARNINGS -DWIN32_LEAN_AND_MEAN -lshell32 -lkernel32 -lmsvcrt -Xlinker /NODEFAULTLIB:libcmt"
 set "D_FLAGS=-g -gcodeview"
 set "R_FLAGS=-DNDEBUG -O2"
 set "file=wavgen"
 
 if /I "%~1" EQU "debug" (
-    set "msg=Building wavgen in DEBUG mode..."
-    set "cmd=%CC% -o %file%.exe %file%.c %FLAGS% %D_FLAGS%"
+    set "mode=DEBUG"
+    set "flags=%FLAGS% %D_FLAGS%"
 ) else (
-    set "msg=Building wavgen in RELEASE mode..."
-    set "cmd=%CC% -o %file%.exe %file%.c %FLAGS% %R_FLAGS%"
+    set "mode=RELEASE"
+    set "flags=%FLAGS% %R_FLAGS%"
 )
 
-echo [1;44m%msg%[0m
+echo [1;44mBuilding wavgen in %mode% mode...[0m
 @echo on
-call %cmd%
+%CC% -o %file%.exe %file%.c %flags%
 @echo off
 
 if /I "%~1" NEQ "debug" (
